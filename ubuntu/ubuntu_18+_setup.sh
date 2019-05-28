836edde3338aaf8209fe9af5402874ae1d7bfb8d
lock14 <lock14@users.noreply.github.com>
2019-05-28 14:23:39 -0700

    Update ubuntu_18+_setup.sh

--- ubuntu/ubuntu_18+_setup.sh
+++ ubuntu/ubuntu_18+_setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# setup automation for java development on Ubunutu version 18.X or later
+# setup automation for java development on Ubuntu version 18.X or later
+
+usage() {
+    echo "usage: $(basename $0) -j [oracle-8-jdk | openjdk-8-jdk | openjdk-11-jdk] -i [intellij | intellij-ultimate | eclipse | netbeans]"
+}
 
 # need to run as root
 if [ "$EUID" -ne 0 ]; then
@@ -8,17 +12,26 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-while getopts ":j:" opt; do
+while getopts ":j:i:h" opt; do
   case ${opt} in
     j )
       JDK_PACKAGE=$OPTARG
       ;;
+    i )
+      IDE=$OPTARG
+      ;;
+    h )
+      usage
+      exit 0
+      ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
+      usage
       exit 1
       ;;
     : )
       echo "Invalid option: $OPTARG requires an argument" 1>&2
+      usage
       exit 1
       ;;
   esac
@@ -30,10 +43,20 @@ if [ "$JDK_PACKAGE" = "" ]; then
     JDK_PACKAGE="openjdk-11-jdk"
 fi
 
+# if no ide provided, default to intellij
+if [ "$IDE" = "" ]; then
+    IDE="intellij"
+fi
+
 # validate choice of jdk package
 if [ "$JDK_PACKAGE" != "oracle-8-jdk" ] && [ "$JDK_PACKAGE" != "openjdk-8-jdk" ] && [ "$JDK_PACKAGE" != "openjdk-11-jdk" ]; then
     echo "$JDK_PACKAGE is not a supported jdk package. Choose from: oracle-8-jdk, openjdk-8-jdk, or openjdk-11-jdk"
     exit 1
+fi
+
+# validate choice of ide
+if [ "$IDE" != "intellij" ] && [ "$IDE" != "intellij-ultimate" ] && [ "$IDE" != "eclipse" ] && [ "$IDE" != "netbeans" ]; then
+    echo "$IDE is not a supported IDE. Chose from: intellij, intellij-ultimate, eclipse, or netbeans"
 fi
 
 # get everything up to date
@@ -42,10 +65,6 @@ apt --yes update
 apt --yes full-upgrade
 # auto remove uneeded things
 apt --yes autoremove
-
-# install git
-echo "installing git"
-apt --yes install git
 
 # install vim
 echo "installing vim"
@@ -63,7 +82,7 @@ apt --yes install mariadb-server mariadb-client
 echo "installing jdk"
 if [ "$JDK_PACKAGE" = "oracle-8-jdk" ]; then
     # add oracle-jdk ppa
-    add-apt-repository ppa:webupd8team/java
+    add-apt --yes-repository ppa:webupd8team/java
     apt --yes update 
     # install oracle jdk 8
     apt --yes install oracle-java8-installer
@@ -81,16 +100,29 @@ fi
 echo "installing maven"
 apt --yes install maven
 
-# install intellij
-echo "intalling intellij-idea-ultimate"
-snap install intellij-idea-ultimate --classic
+if [ "$IDE" = "intellij" ]; then
+    echo "intalling intellij-idea-community"
+    snap install intellij-idea-community --classic
+elif [ "$IDE" = "intellij-ultimate" ]; then
+    # install intellij ultimate
+    echo "intalling intellij-idea-ultimate"
+    snap install intellij-idea-ultimate --classic
+elif [ "$IDE" = "eclipse" ]; then
+    # install eclipse 
+    echo "installing eclipse"
+    snap install eclipse --classic
+elif [ "$IDE" = "netbeans" ]; then
+    echo "installing netbeans"
+    snap install netbeans --classic
+fi
 
 # install postman
 echo "intalling postman"
 snap install postman
 
+echo "installing slack"
+snap install slack --classic
+
 echo "Recommended Tools"
 echo "Database Management - DbVisualizer: https://www.dbvis.com/download/10.0"
 
-echo "rebooting..."
-systemctl reboot
