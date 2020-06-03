8f358c6155e63d4146eac7c621d87bdb87a44b55
lock14 <lock14@users.noreply.github.com>
2020-06-02 18:02:16 -0700

    Update fedora_presteps.sh

--- fedora/fedora_presteps.sh
+++ fedora/fedora_presteps.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# need to run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "No root permission. Please run using 'sudo'"
-    exit 1
-fi
-
 echo "updating system"
-dnf -y upgrade --refresh
+sudo dnf -y upgrade --refresh
 
 echo "installing snapd"
-dnf -y install snapd
-ln -s /var/lib/snapd/snap /snap
-echo "PATH=\$PATH:/var/lib/snapd/snap/bin" >> /etc/bashrc
+sudo dnf -y install snapd
+sudo ln -s /var/lib/snapd/snap /snap
 
 echo "system restarting in 30 seconds..."
 sleep 30
